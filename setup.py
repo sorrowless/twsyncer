9b1bb646f8510694ecf661352c08dd6794255465
Stan Bogatkin <stabog.tmb@gmail.com>
2020-05-10 00:29:25 +0300

    Add first version of package buildable on pypi
    Add needed info to make package pushable to PyPi.

--- setup.py
+++ setup.py
@@ -5,17 +5,22 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="twsyncer",
-    version="0.0.2",
+    version="0.0.3",
     author="Stan Bogatkin",
     author_email="sbog@sbog.ru",
     description="Tool to sync Taswarrior and remote task trackers",
+    entry_points={
+        "console_scripts": [
+            "twsyncer = twsyncer.main:main",
+        ]
+    },
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/twsyncer",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache 2 License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
--- twsyncer/main.py
+++ twsyncer/main.py
@@ -1,43 +1,49 @@
-from ghub import GithubWorker
-from ghub import TaskWarriorWorker
-from worker import Worker
+from .ghub import GithubWorker
+from .twarrior import TaskWarriorWorker
+from .worker import Worker
 
 import os
 
-dry_run = os.environ.get('TW_DRY_RUN', True)
-# Actually you getting str from env, let's roughly convert it
-dry_run = False if dry_run == 'False' else True
-token = os.environ.get('TW_GITHUB_TOKEN', False)
 
-org_login = os.environ.get('TW_GITHUB_ORG_LOGIN', "")
-project_name = os.environ.get('TW_GITHUB_PROJECT', "")
-issues_repo_name = os.environ.get('TW_GITHUB_ISSUES_REPO', "")
-# We somehow should map Github project columns to states in Taskwarrior. We
-# could use UDA but actually it is way easier to use some mapping to the
-# existing in Taskwarrior states
-tw_to_github_states = {
-    'pending': ['Backlog', 'To do'],
-    'active': ['In progress'],
-    'completed': ['Done', 'Cancelled', 'Feedback Needed']
-}
+def main():
+    dry_run = os.environ.get('TW_DRY_RUN', True)
+    # Actually you getting str from env, let's roughly convert it
+    dry_run = False if dry_run == 'False' else True
+    token = os.environ.get('TW_GITHUB_TOKEN', False)
 
-gh_worker = GithubWorker(
-    dry_run=dry_run,
-    token=token,
-    org_login=org_login,
-    project_name=project_name,
-    issues_repo_name=issues_repo_name
-)
-gh_worker.initialize()
-gh_worker.get_issues()
-gh_worker.set_states_mapping(tw_to_github_states)
+    org_login = os.environ.get('TW_GITHUB_ORG_LOGIN', "")
+    project_name = os.environ.get('TW_GITHUB_PROJECT', "")
+    issues_repo_name = os.environ.get('TW_GITHUB_ISSUES_REPO', "")
+    # We somehow should map Github project columns to states in Taskwarrior. We
+    # could use UDA but actually it is way easier to use some mapping to the
+    # existing in Taskwarrior states
+    tw_to_github_states = {
+        'pending': ['Backlog', 'To do'],
+        'active': ['In progress'],
+        'completed': ['Done', 'Cancelled', 'Feedback Needed']
+    }
 
-# Now start working with taskwarrior
-tw_filter_project = os.environ.get('TW_FILTER_PROJECT', "")
-tw_worker = TaskWarriorWorker(
-    filter_project=tw_filter_project
-)
-tw_worker.initialize()
+    gh_worker = GithubWorker(
+        dry_run=dry_run,
+        token=token,
+        org_login=org_login,
+        project_name=project_name,
+        issues_repo_name=issues_repo_name
+    )
+    gh_worker.initialize()
+    gh_worker.get_issues()
+    gh_worker.set_states_mapping(tw_to_github_states)
 
-worker = Worker(gh_worker, tw_worker)
-worker.resolve()
+    # Now start working with taskwarrior
+    tw_filter_project = os.environ.get('TW_FILTER_PROJECT', "")
+    tw_worker = TaskWarriorWorker(
+        filter_project=tw_filter_project
+    )
+    tw_worker.initialize()
+
+    worker = Worker(gh_worker, tw_worker)
+    worker.resolve()
+
+
+if __name__ == "__main__":
+    main()
